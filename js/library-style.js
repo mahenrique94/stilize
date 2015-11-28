205bdd16822b47251a41b62a892e100540f42ae6
Matheus Henrique <matheus@Matheuss-MacBook-Pro.local>
2015-11-28 13:47:37 -0200

    Implementando formulario, inputs e icons

--- js/library-style.js
+++ js/library-style.js
@@ -1,3 +1,16 @@
+jQuery(document).ready(function () {
+	
+	/* @auth Matheus
+	 * ESCONDER ELEMENTOS DEPOIS DE 2 SEGUNDOS
+	 */
+	if (!jQuery('.time-out').is(':empty')) {
+		setTimeout(function () {
+           jQuery('.time-out').toggle(); 
+        }, 2000);
+    }
+	
+});
+
 function toggleElement(element, parametro) {
 	jQuery(element + '[data-toggle=' + parametro + ']').fadeToggle("slow");
 }
