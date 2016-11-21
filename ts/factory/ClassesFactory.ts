8ca8c87ed6755276b03db7ad8fc14969c5c725f1
Matheus <matheushcastiglioni@gmail.com>
2016-11-21 10:28:32 -0200

    Atualizando repositório para a nova versão do framework

--- ts/factory/ClassesFactory.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../controller/ModalController.ts"/>
-
-(function(classes) {
-
-    classes.ModalController = ModalController;
-    
-})(window.classes || (window.classes = {}));
--- ts/factory/ControllerFactory.ts
+++
@@ -1,8 +0,0 @@
-(function($, win) {
-    $.addEventListener('DOMContentLoaded', function() {
-        $.querySelectorAll('[data-controller]').forEach(function(element) {
-            let clazz = element.dataset.controller;
-            win[clazz] = new win.classes[clazz]();
-        });
-    });
-})(document, window);
