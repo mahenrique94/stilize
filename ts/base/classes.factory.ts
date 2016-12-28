7c401652fbbf188ad33de755d65b3900fc554338
Matheus <matheushcastiglioni@gmail.com>
2016-12-27 23:12:24 -0200

    Inicio das midias para HTML5

--- ts/base/classes.factory.ts
+++ ts/base/classes.factory.ts
@@ -1,5 +1,6 @@
 /// <reference path="../component/nav/nav.controller.ts"/>
 /// <reference path="../component/tab/tab.controller.ts"/>
+/// <reference path="../component/media/media.controller.ts"/>
 /// <reference path="../object/modal/modal.controller.ts"/>
 /// <reference path="../object/steps/steps.controller.ts"/>
 
@@ -9,5 +10,6 @@
     classes.NavController = NavController;
     classes.TabController = TabController;
     classes.StepsController = StepsController;
+    classes.MediaController = MediaController;
 
 })(window.classes || (window.classes = {}));
