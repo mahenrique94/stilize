1c258a1b3364d25ae6e645884604bdd40a6a5253
Matheus <matheushcastiglioni@gmail.com>
2016-12-30 20:47:22 -0200

    Criado object mark para checkbox e radios

---
+++ ts/object/mark/mark.controller.ts
@@ -0,0 +1,19 @@
+/// <reference path="./mark.object.ts"/>
+
+class MarkController {
+
+    private _mark : Mark = new Mark();
+
+    public mark(mark : HTMLObjectElement) : void {
+        this._mark.mark(mark);
+    }
+
+    let marks = document.querySelectorAll('.o-mark__data');
+    marks.forEach(mark => {
+        if (mark.checked) {
+            let element = mark.parentNode.querySelector('.o-mark__element');
+            element.classList.add('is-marked');
+        }
+    });
+
+}
